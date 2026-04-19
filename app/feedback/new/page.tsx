6fca01ba87d7a3602a39f2f4950c916df4e61398
Richwell111 <bcict20103@gmail.com>
2026-04-19 15:46:24 +0000

    feat: implement new feedback submission page with server action and toast notifications

--- app/feedback/new/page.tsx
+++ app/feedback/new/page.tsx
@@ -11,7 +11,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { error } from "console";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
